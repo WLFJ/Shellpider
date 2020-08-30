860ea14072ce0bb898456cdf35e4d77fea031bde
Lazy Wind Lazy Hurry <49600975+WLFJ@users.noreply.github.com>
2020-08-30 12:57:15 +0800

    Revert "Reorgan"

--- website_shell/00-template.sh
+++ website_shell/00-template.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
 
-INFO="中北大学-教务动态"
-URL="http://jwc.nuc.edu.cn/index/jwdt.htm"
-FIRST_PARSE='<a class="c54561".*>'
-SECOND_LINK_PARSE='s/.*href="\(.*\)" target="_blank" title="\(.*\)">$/\1/'
-SECOND_TITLE_PARSE='s/.*href="\(.*\)" target="_blank" title="\(.*\)">$/\2/'
+# insert your website title here
+INFO=""
 
-source "../parse.sh"
+if [[ $1 == "GET_INFO" ]]; then
+	echo ${INFO}
+	exit 0
+fi
+
+# website URL
+URL=""
+DOMAIN_URL=$(echo ${URL} | sed 's/\(http:\/\/[^/]*\/\).*/\1/')
+SUB_URL=$(echo ${URL} | sed 's/\(http:\/\/[^/]*\/[^/]*\/\).*/\1/')
+
+IS_OK=0
+
+curl -s ${URL}| while read LINE 
+# cat res.txt | while read LINE 
+do
+	# Change inorder to find exactly title HTML-expr.
+	TMP_TITLE=$(grep -oE '<a class="demo".*>' <<< ${LINE})
+	RES_CODE=${?}
+	if [ $RES_CODE -eq 0 ];then
+		LINK_URL=$(echo ${TMP_TITLE} | sed 's/.*href="\(.*\)" target="_blank" title="\(.*\)">$/\1/')
+		TITLE=$(echo ${TMP_TITLE} | sed 's/.*href="\(.*\)" target="_blank" title="\(.*\)">$/\2/')
+		if [[ ${LINK_URL:0:2} == ".." ]];then
+			LINE_URL="${DOMAIN_URL}${LINK_URL:3}"
+		else
+			LINE_URL="${SUB_URL}${LINK_URL}"
+		fi
+		echo "[${LINE_URL}] [${TITLE}]"
+		IS_OK=1
+		break
+	fi
+done
+if [ $IS_OK -eq 0 ]; then
+	exit 0
+else
+	exit 1
+fi
+
